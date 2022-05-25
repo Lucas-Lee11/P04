2dd7c234b12604575aa077b3e8a57f53c0a7f7f4
Andrew <andrewjuang01@gmail.com>
2022-05-25 09:24:30 -0400

    started teacher, student, schedule tables

--- app/database.py
+++ app/database.py
@@ -9,19 +9,27 @@ DB_FILE = "loophole.db"
 
 
 class Student:
+
     @staticmethod
-    def create_student(email: str) -> None:
+    def create_db():
         with sqlite3.connect(DB_FILE) as db:
             c = db.cursor()
             c.execute(
                 """
                 CREATE TABLE IF NOT EXISTS students(
-                    student_id  TEXT PRIMARY KEY DEFAULT (hex(randomblob(8))),
-                    email       TEXT
+                    student_id          TEXT PRIMARY KEY DEFAULT (hex(randomblob(8))),
+                    email               TEXT,
+                    starredteachers     INTEGER,
+                    FOREIGN KEY(starredteachers) REFERENCES teachers(teacher_id)
                 )
                 """
             )
-            
+            db.commit()
+
+    @staticmethod
+    def create_student(email: str) -> None:
+        with sqlite3.connect(DB_FILE) as db:
+            c = db.cursor()
             # some kind of validation? also need more info about what OAuth sends back
 
             c.execute(
@@ -40,3 +48,55 @@ class Student:
             if student_id is not None:
                 return student_id[0]
             return None
+
+
+class Teacher:
+
+    @staticmethod
+    def create_db():
+        with sqlite3.connect(DB_FILE) as db:
+            c = db.cursor()
+            c.execute(
+                """
+                CREATE TABLE IF NOT EXISTS teachers(
+                    teacher_id  TEXT PRIMARY KEY DEFAULT (hex(randomblob(8))),
+                    name        TEXT,
+                    email       TEXT,
+                    pronouns    TEXT,
+                    title       TEXT,
+                    schedules   INTEGER,
+                    FOREIGN KEY(schedules) REFERENCES schedules(schedule_id)
+                )
+                """
+            )
+            db.commit()
+
+    def create_teacher(self, email: str) -> None:           
+            # some kind of validation? also need more info about what OAuth sends back
+
+            c.execute(
+                "INSERT INTO teachers(email) VALUES (?)", (email)
+            )
+
+
+class Schedules:
+
+    @staticmethod
+    def create_db():
+        with sqlite3.connect(DB_FILE) as db:
+            c = db.cursor()
+            c.execute(
+                """
+                CREATE TABLE IF NOT EXISTS schedules(
+                    schedule_id  TEXT PRIMARY KEY DEFAULT (hex(randomblob(8))),
+                )
+                """
+            )
+            db.commit()
+
+
+# TESTING
+# teacher = Teacher()
+# teacher.create_db()
+# student = Student()
+# student.create_db()
