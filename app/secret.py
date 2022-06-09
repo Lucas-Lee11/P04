3faa950616460dffc41931ff08383c4ae3c0d70e
Christopher Liu <christopherliu@sorcero.com>
2022-06-09 13:21:41 -0400

    Relative key path

--- app/secret.py
+++ app/secret.py
@@ -1,8 +1,9 @@
 import os
 import pathlib
 
+
 def setup():
     path = os.path.join(pathlib.Path(__file__).parent, "keys/key_oauth2.txt")
-    with open("app/keys/key_oauth2.txt") as f:
+    with open(path) as f:
         GOOGLE_CLIENT_ID = f.readline().strip()
         return GOOGLE_CLIENT_ID
